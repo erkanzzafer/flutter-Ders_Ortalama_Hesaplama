b58842913b43403cafac33a4ed9787089ad3c0ed
erkanzzafer <zzafererkann@gmail.com>
2022-02-11 10:36:05 +0300

    proje

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:dinamik_ortalama_hesapla/constants/app_constants.dart';
+import 'package:dinamik_ortalama_hesapla/ortalamahesapla_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "Dinamik Not Ortalama Hesapla",
+      theme: ThemeData(
+        primarySwatch: Sabitler.anaRenk,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: OrtalamaHesapla(),
+    );
+  }
+}
---
+++ lib/ortalamahesapla_page.dart
@@ -0,0 +1,189 @@
+import 'package:dinamik_ortalama_hesapla/constants/app_constants.dart';
+import 'package:dinamik_ortalama_hesapla/ders_listesi.dart';
+import 'package:dinamik_ortalama_hesapla/helper/data_helper.dart';
+import 'package:dinamik_ortalama_hesapla/model/ders.dart';
+import 'package:dinamik_ortalama_hesapla/ortalama_goster.dart';
+import 'package:flutter/material.dart';
+
+class OrtalamaHesapla extends StatefulWidget {
+  const OrtalamaHesapla({Key? key}) : super(key: key);
+
+  @override
+  _OrtalamaHesaplaState createState() => _OrtalamaHesaplaState();
+}
+
+class _OrtalamaHesaplaState extends State<OrtalamaHesapla> {
+  GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  double secilenHarfDeger = 4;
+  double secilenKrediDeger = 1;
+  String girilenDersAdi = "";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        centerTitle: true,
+        title: Text(
+          Sabitler.baslikText,
+          style: Sabitler.baslikStyle,
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          //form
+          Row(
+            children: [
+              Expanded(
+                flex: 2,
+                child: _buildForm(),
+              ),
+              Expanded(
+                flex: 1,
+                child: OrtalamaGoster(
+                    dersSayisi: dataHelper.tumEklenenDersler.length,
+                    ortalama: dataHelper.ortalamaHesapla()),
+              )
+            ],
+          ),
+
+          Expanded(
+            child: DersListesi(),
+            
+          ),
+          //liste
+        ],
+      ),
+    );
+  }
+
+  Widget _buildForm() {
+    return Form(
+      key: formKey,
+      child: Column(
+        children: [
+          Padding(
+            padding: Sabitler.yatayPadding8,
+            child: _buildTextFormField(),
+          ),
+          SizedBox(height: 5),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: Sabitler.yatayPadding8,
+                  child: _buildHarfler(),
+                ),
+              ),
+              Expanded(
+                child: Padding(
+                  padding: Sabitler.yatayPadding8,
+                  child: _buildKrediler(),
+                ),
+              ),
+              IconButton(
+                onPressed: _dersEkleveOrtalamaHesapla,
+                icon: Icon(Icons.arrow_forward_ios),
+                color: Sabitler.anaRenk,
+                iconSize: 30,
+              ),
+            ],
+          ),
+          SizedBox(
+            height: 5,
+          ),
+        ],
+      ),
+    );
+  }
+
+  _buildTextFormField() {
+    return TextFormField(
+      onSaved: (deger) {
+        setState(() {
+          girilenDersAdi = deger!;
+        });
+      },
+      validator: (s) {
+        if (s!.length <= 0) {
+          return 'Ders Adı giriniz';
+        } else {
+          return null;
+        }
+      },
+      decoration: InputDecoration(
+        hintText: "Matematik",
+        border: OutlineInputBorder(
+          borderRadius: Sabitler.borderRadius,
+          borderSide: BorderSide.none,
+        ),
+        filled: true,
+        fillColor: Sabitler.anaRenk.shade100.withOpacity(0.2),
+      ),
+    );
+  }
+
+  _buildHarfler() {
+    return Container(
+      alignment: Alignment.center,
+      padding: Sabitler.dropDownPadding,
+      decoration: BoxDecoration(
+        color: Sabitler.anaRenk.shade300.withOpacity(0.3),
+        borderRadius: Sabitler.borderRadius,
+      ),
+      child: DropdownButton<double>(
+        value: secilenHarfDeger,
+        elevation: 16,
+        iconEnabledColor: Sabitler.anaRenk.shade200,
+        onChanged: (deger) {
+          setState(() {
+            secilenHarfDeger = deger!;
+            print(secilenHarfDeger);
+          });
+        },
+        underline: Container(),
+        items: dataHelper.tumDersleriHarfleri(),
+      ),
+    );
+  }
+
+  _buildKrediler() {
+    return Container(
+      alignment: Alignment.center,
+      padding: Sabitler.dropDownPadding,
+      decoration: BoxDecoration(
+          color: Sabitler.anaRenk.shade300.withOpacity(0.3),
+          borderRadius: Sabitler.borderRadius),
+      child: DropdownButton<double>(
+        value: secilenKrediDeger,
+        elevation: 16,
+        iconEnabledColor: Sabitler.anaRenk.shade200,
+        onChanged: (deger) {
+          setState(() {
+            secilenKrediDeger = deger!;
+            print(secilenKrediDeger);
+          });
+        },
+        underline: Container(),
+        items: dataHelper.tumDerslerinKredileri(),
+      ),
+    );
+  }
+
+  void _dersEkleveOrtalamaHesapla() {
+    if (formKey.currentState!.validate()) {
+      formKey.currentState!.save();
+      var eklenecekDers = Ders(
+          ad: girilenDersAdi,
+          harfDegeri: secilenHarfDeger,
+          krediDegeri: secilenKrediDeger);
+      //  print(eklenecekDers);
+      dataHelper.dersEkle(eklenecekDers);
+      print(dataHelper.tumEklenenDersler);
+      print(dataHelper.ortalamaHesapla());
+      setState(() {});
+    }
+  }
+}
